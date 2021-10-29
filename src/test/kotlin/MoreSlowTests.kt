37a4c81886136bc531b87378b764ee6785cc3a13
evgeny.bovykin <evgeny.bovykin@jetbrains.com>
2021-10-29 11:34:16 +0200

    Add more slow tests

---
+++ src/test/kotlin/MoreSlowTests.kt
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.random.Random
+import kotlin.test.assertEquals
+
+class MoreSlowTests {
+    private val testDuration = 15000L
+
+    @Test
+    @Tag("slow")
+    fun `this is a slow test`() {
+        Thread.sleep(testDuration)
+    }
+
+    @Test
+    @Tag("slow")
+    fun `this is another slow test`() {
+        Thread.sleep(testDuration)
+    }
+
+    @Test
+    @Tag("slow")
+    fun `this is a slow test that fails`() {
+        Thread.sleep(testDuration)
+        assertEquals(2, 1)
+    }
+
+    @Test
+    @Tag("slow")
+    fun `and this is yet another slow test`() {
+        Thread.sleep(testDuration)
+    }
+}
